7336eb77cceb86520f4ee523e8c24b22c7829e46
dylanbatar <dylanbatar2@gmail.com>
2020-11-20 14:51:52 -0500

    setup project finish

---
+++ README.md
@@ -0,0 +1,16 @@
+# Store frontend
+
+this project is inspirate on the web [cartzilla grocery store](https://demo.createx.studio/cartzilla/home-grocery-store.html)
+
+# Tecnologies
+
+- React
+- Redux
+- Formilk
+- Tailwind
+
+# How to start
+
+1. run on the command line `npm install` or `npm i` for install dependencies of the project
+2. write `npm start`
+3. open your browser with url `localhost:3000`
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { StoreApp } from './StoreApp';
+// import reportWebVitals from './reportWebVitals';
+import './styles/tailwind.css';
+import './styles/normalize.css';
+ReactDOM.render(<StoreApp />, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
